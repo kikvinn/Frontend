1aea6f7f355a19df5f12d3d89552c035fb694e93
student <student@marsu.ru>
2024-12-04 19:50:37 +0300

    пара 04.12.24

---
+++ scripts/swipper.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    var swiper = new Swiper('.swiper', {
+        slidesPerView: 3,
+        spaceBetween: 10,
+        autoHeight: true
+    });
+});
